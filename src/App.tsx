99d6f6d1a302b47014b483f210fb2515a6201ad6
francdenis2026-source <francdenis2026@gmail.com>
2026-08-09 22:12:00 -0500

    Ativa fluxo claro de busca e comparação

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PrecoCertoApp from "./PrecoCertoApp";
 import { MaxPriceStoreLabels } from "./components/MaxPriceStoreLabels";
+import { SearchUxClarity } from "./components/SearchUxClarity";
 
 export default function App() {
   return (
     <BrowserRouter>
       <MaxPriceStoreLabels />
+      <SearchUxClarity />
       <Routes>
         <Route path="*" element={<PrecoCertoApp />} />
       </Routes>
