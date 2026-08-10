eda13da7a554e85f2691b5b04baa79ed3d5bb33a
francdenis2026-source <francdenis2026@gmail.com>
2026-08-10 10:14:58 -0500

    feat: expose merchant, customer and platform admin routes

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,10 @@ import { HomeSearchKeyboardUx } from "./components/HomeSearchKeyboardUx";
 import { FooterCompactUx } from "./components/FooterCompactUx";
 import { RadarShowcaseUx } from "./components/RadarShowcaseUx";
 import { PreferredProductPngUpgrade } from "./components/PreferredProductPngUpgrade";
+import { MerchantDashboard } from "./components/MerchantDashboard";
+import { PlatformAdminDashboard } from "./components/PlatformAdminDashboard";
+import { CustomerOrders } from "./components/CustomerOrders";
+import { MercadoPagoCallback } from "./components/MercadoPagoCallback";
 import { CollaboratePage, ContactPage, MerchantSignupPage, PharmaciesPage } from "./components/PublicFooterServicePages";
 
 export default function App() {
@@ -33,6 +37,10 @@ export default function App() {
       <PreferredProductPngUpgrade />
       <Routes>
         <Route path="/lojista" element={<MerchantSignupPage />} />
+        <Route path="/painel-lojista" element={<MerchantDashboard />} />
+        <Route path="/meus-pedidos" element={<CustomerOrders />} />
+        <Route path="/integracoes/mercadopago/callback" element={<MercadoPagoCallback />} />
+        <Route path="/admin/plataforma" element={<PlatformAdminDashboard />} />
         <Route path="/colaborar" element={<CollaboratePage />} />
         <Route path="/fale-conosco" element={<ContactPage />} />
         <Route path="/farmacias" element={<PharmaciesPage />} />
