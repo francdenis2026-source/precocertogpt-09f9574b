bf4c4cdddd7d949a0cba54344611172c9a9283cf
francdenis2026-source <francdenis2026@gmail.com>
2026-08-09 22:02:31 -0500

    Ativa identificação da loja com maior preço

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PrecoCertoApp from "./PrecoCertoApp";
+import { MaxPriceStoreLabels } from "./components/MaxPriceStoreLabels";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <MaxPriceStoreLabels />
       <Routes>
         <Route path="*" element={<PrecoCertoApp />} />
       </Routes>
